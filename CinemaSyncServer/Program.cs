e0d7fc2a41934cbfcba3073ba52285805c8d3f9c
OrAlperovich <or6661@gmail.com>
2026-04-20 17:44:43 +0300

    After Hagana

--- CinemaSyncServer/Program.cs
+++ CinemaSyncServer/Program.cs
@@ -7,6 +7,9 @@ builder.Services.AddControllers();
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
+builder.Services.AddHttpClient();
+builder.Services.AddSingleton<CinemaSyncServer.Services.MapiService>();
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
