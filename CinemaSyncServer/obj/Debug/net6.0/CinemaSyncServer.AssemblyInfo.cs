6ac762a8a40ab6a744c26babc697f0dd762ac7d8
OrAlperovich <or6661@gmail.com>
2026-04-20 17:44:43 +0300

    After Hagana

--- CinemaSyncServer/obj/Debug/net6.0/CinemaSyncServer.AssemblyInfo.cs
+++ CinemaSyncServer/obj/Debug/net6.0/CinemaSyncServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaSyncServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b6edf0c6756d2115d36235d6e9055fcc8029a46d")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a902ab86b63bc7134c18295de83dfeeee4a7d611")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaSyncServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaSyncServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
