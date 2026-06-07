00a9610d50efc30e46214445b2e6d2552d61c36d
group33GAO <33groupgao@gmail.com>
2026-06-07 21:03:50 +0300

    Added Dark/Bright mode

--- CinemaSyncServer/obj/Debug/net6.0/CinemaSyncServer.AssemblyInfo.cs
+++ CinemaSyncServer/obj/Debug/net6.0/CinemaSyncServer.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("CinemaSyncServer")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9d145950a36acb60577a407cd1fd04df0c440113")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e9ed5d791332951d662a1eacf0c060ffc46d36b5")]
 [assembly: System.Reflection.AssemblyProductAttribute("CinemaSyncServer")]
 [assembly: System.Reflection.AssemblyTitleAttribute("CinemaSyncServer")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
